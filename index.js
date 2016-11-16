426c8c11e7199107618b1568aaf2f9e2cd84fecd
imatvieiev <idmatvieiev@gmail.com>
2016-11-16 21:19:53 +0200

    1.3.0

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-
+//     jstring
+//
+//     Copyright (c) 2015 Ivan Matvieiev
+//     MIT License - http://opensource.org/licenses/mit-license.php
 'use strict';
-function JString() {
-    /**
-     * @method
-     * @name stringChecker
-     * @access private
-     * @description Throws a TypeError when incoming parameter is not of type string.
-     * @param {string} string incoming string.
-     */
-    let stringChecker = function(string){
-        if(typeof string !== `string`){
-            throw new TypeError(`First parameter must be a string`);
-        }
-    };
-    /**
-     * @method
-     * @name numberChecker
-     * @access private
-     * @description Throws a TypeError when incoming parameter is not of type number.
-     * @param {number} number incoming number.
-     */
-    let numberChecker = function(number){
-        if(typeof number !== `number`){
-            throw new TypeError(`First parameter must be a number`);
-        }
-    };
-    this.removeTags = function (string, except) {
+
+/**
+* @method
+* @name stringChecker
+* @access private
+* @description Throws a TypeError when incoming parameter is not of type string.
+* @param {string} string incoming string.
+*/
+let stringChecker = string => {
+    if(typeof string !== `string`){
+        throw new TypeError(`First parameter must be a string`);
+    }
+};
+
+/**
+* @method
+* @name numberChecker
+* @access private
+* @description Throws a TypeError when incoming parameter is not of type number.
+* @param {number} number incoming number.
+*/
+let numberChecker = number => {
+    if(typeof number !== `number`){
+        throw new TypeError(`First parameter must be a number`);
+    }
+};
+
+const jstring = {
+    removeTags(string, except) {
         stringChecker(string);
+        if (!Array.isArray(except)) {
+            throw new TypeError(`Except parameter must be an array`);
+        }
         if (except) {
             let closingTag = except.map(function (elem) {
                 return `\/${elem}`;
@@ -38,66 +47,66 @@ function JString() {
         }
         let reg = new RegExp(`(<|&lt;)(${except}).*?(>|&gt;)`, `g`);
         return typeof string === `string` ? string.replace(reg, ``) : false;
-    };
-    this.cut = function (string, len) {
+    },
+    cut(string, len) {
         stringChecker(string);
         numberChecker(len);
         return string.substr(0, len);
-    };
-    this.capitalize = function (string) {
+    },
+    capitalize(string) {
         stringChecker(string);
         return string.charAt(0).toUpperCase() + string.slice(1);
-    };
-    this.toLow = function (string) {
+    },
+    toLow(string) {
         stringChecker(string);
         return string.toLowerCase();
-    };
-    this.toUp = function (string) {
+    },
+    toUp(string) {
         stringChecker(string);
         return string.toUpperCase();
-    };
-    this.minifyWhitespace = function (string) {
+    },
+    minifyWhitespace(string) {
         stringChecker(string);
         return string.replace(/\s{2,}/g, ` `);
-    };
-    this.replaceAll = function (string, props){
+    },
+    replaceAll(string, props){
         stringChecker(string);
         let flag = props[2] ? `g`: `gi`;
         return string.replace(new RegExp(props[0], flag), props[1]);
-    };
-    this.replace = function (string, props){
+    },
+    replace(string, props){
         stringChecker(string);
         let flag = props[2] ? ``: `i`;
         return string.replace(new RegExp(props[0], flag), props[1]);
-    };
-    this.left = function (string, len){
+    },
+    left(string, len){
         stringChecker(string);
         numberChecker(len);
         return string.substr(0, len);
-    };
-    this.right = function (string, len){
+    },
+    right(string, len){
         stringChecker(string);
         numberChecker(len);
         return string.substr(string.length-len);
-    };
-    this.forEach = function (string, fn){
+    },
+    forEach(string, fn){
         stringChecker(string);
         let i=0,
             length = string.length;
         for(;i<length;i++){
             fn.call(string, string[i], i);
         }
-    };
-    this.repeat = function (string, n){
-      stringChecker(string);
-      numberChecker(n);
-      let result = string;
-      for(let i=1; i<n; i++){
-        result+=string;
-      }
-      return result;
-    };
-    this.handleString = function (string, task) {
+    },
+    repeat(string, n){
+        stringChecker(string);
+        numberChecker(n);
+        let result = string;
+        for(let i=1; i<n; i++){
+            result+=string;
+        }
+        return result;
+    },
+    handleString(string, task) {
         stringChecker(string);
         if (typeof task === `object`) {
             for (let fun in task) {
@@ -108,28 +117,26 @@ function JString() {
             return string;
         }
         return false;
-    };
-    this.isAlpha = function (string) {
+    },
+    isAlpha(string) {
         stringChecker(string);
         return /^[a-zA-Z]*$/.test(string);
-    };
-    this.isAlphanumeric = function (string) {
+    },
+    isAlphanumeric(string) {
         stringChecker(string);
         return /^[a-zA-Z0-9]+$/.test(string);
-    };
-    this.isEmail = function (string) {
+    },
+    isEmail(string) {
         stringChecker(string);
         return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(string);
-    };
-    this.isUpper = function (string, strict) {
+    },
+    isUpper(string, strict) {
         stringChecker(string);
         return strict ? /^[A-Z0-9 _-]+$/.test(string): /[A-Z]+/.test(string);
-    };
-    this.isLower = function (string, strict) {
+    },
+    isLower(string, strict) {
         stringChecker(string);
         return strict ? /^[a-z0-9 _-]+$/.test(string): /[a-z]+/.test(string);
-    };
-
-
-}
-module.exports = new JString();
+    },
+};
+module.exports = jstring;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,19 @@
 'use strict';
-let S = require(`../index.js`);
 
-console.log(S.right('12345', 3));
+const jstring = require('../');
+const assert = require('assert');
+
+describe('string rules', () => {
+    it('Should capitalize string', () => {
+        assert.equal(jstring.capitalize('jstring'), 'Jstring');
+    });
+    it('Should capitalize string', () => {
+        assert.equal(jstring.capitalize('Jstring'), 'Jstring');
+    });
+    it('Should remove tags', () => {
+        assert.equal(jstring.removeTags('Jstring<tag>'), 'Jstring');
+    });
+    it('Should remove tags with exception', () => {
+        assert.equal(jstring.removeTags('Jstring<tag><foo>', ['foo']), 'Jstring<foo>');
+    });
+});
